d8444a7776d29162048db005a8b83ea3095d41ac
Apurva Nasar <apurvanasar7@gmail.com>
2024-11-04 21:59:28 +0530

    Model Trainer

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -1,10 +1,16 @@
 import os
 import sys
-from src.mlproject.exception import CustomException
+#from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
+import numpy as np
+import dill
 from dotenv import load_dotenv
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 import pymysql
+
+from src.mlproject.exception import CustomException
 
 import pickle
 import numpy as np
@@ -26,7 +32,7 @@ def read_sql_data():
             db=db
             
         )
-        logging.info(f"Connection Established with {db}")
+        logging.info("Connection Established",mydb)
         df=pd.read_sql_query('Select * from students',mydb)
         print(df.head())
         
@@ -35,7 +41,7 @@ def read_sql_data():
         
         
     except Exception as ex:
-        raise CustomException(ex,sys)
+        raise CustomException(ex)
     
     
 def save_object(file_path, obj):
@@ -49,3 +55,39 @@ def save_object(file_path, obj):
             
     except Exception as e:
         raise CustomException(e, sys)
+    
+            
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+        
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            
+            
+            gs = GridSearchCV(estimator=model, param_grid=para, error_score='raise')
+            gs.fit(X_train,y_train)
+            
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            
+            #model.fit(X_train, y_train)   #Train model
+            
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            
+            train_model_score = r2_score(y_train, y_train_pred)
+            
+            test_model_score = r2_score(y_test,y_test_pred)
+            
+            report[list(models.keys())[i]] = test_model_score
+            
+        return report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+    #def load_object(file_path):
+        #try:           
+            
